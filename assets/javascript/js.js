7737284e32ff57639ebeff277ed9523dc081914f
daryllpenafiel <daryll_penafiel@yahoo.com>
2018-08-16 00:19:52 -0400

    halfway done

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -38,4 +38,13 @@ $(document).ready(function () {
         }
     })
 
+    (function($) {
+        "use strict";
+    
+        // manual carousel controls
+        $('.next').click(function(){ $('.carousel').carousel('next');return false; });
+        $('.prev').click(function(){ $('.carousel').carousel('prev');return false; });
+        
+    })(jQuery);
+
 });
